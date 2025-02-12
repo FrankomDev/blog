2a73b3ab5e0691e1c6df58eb64d9e25f7441cc96
FrankomDev <vasylopozdro@gmail.com>
2025-02-12 07:56:53 +0100

    gitignore test

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template, request, redirect
 import sqlite3
 import hashlib
 
-def hashInput(input):
-    hash = hashlib.sha256(input.encode('utf8')).hexdigest()
-    return hash
-
 def getDB():
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
@@ -34,7 +30,8 @@ def login():
 @app.route('/submit', methods=['POST'])
 def submit():
     password_input = request.form['password_input']
-    if hashInput(password_input) == str(getDB()):
+    hash = hashlib.sha256(password_input.encode('utf8')).hexdigest()
+    if hash == str(getDB()):
         return 'good!'
     else:
         return redirect('/login?error=1', code=302)
