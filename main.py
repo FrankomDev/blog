a9418d6b6789972ad66c9679bdd52eda1eb8da35
FrankomDev <vasylopozdro@gmail.com>
2025-02-13 10:24:28 +0100

    posting blogs

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 import sqlite3
 import hashlib
 import os
+import json
 
 def getDB():
     conn = sqlite3.connect('database.db')
@@ -17,6 +18,17 @@ def countFiles():
     files = [f for f in os.listdir('./static/posts') if os.path.isfile(os.path.join('./static/posts', f))]
     num_files = len(files)
     return num_files
+
+def saveJSON(name,description,date,contains):
+    dictionary = {
+        "name": name,
+        "description": description,
+        "date": date,
+        "contains": contains 
+    }
+    filename = './static/posts/'+str(countFiles()+1)+'.json'
+    with open(filename, "w") as outfile:
+        json.dump(dictionary, outfile)
 
 app = Flask(__name__)
 
@@ -41,6 +53,14 @@ def submit():
     else:
         return redirect('/login?error=1', code=302)
    
+@app.route('/publish', methods=['POST']) 
+def publish():
+    title_input = request.form['title_input']
+    description_input = request.form['description_input']
+    date_input = request.form['date_input']
+    content_input = request.form['content_input']
+    saveJSON(title_input, description_input, date_input, content_input)
+    return redirect('/post?blog='+str(countFiles()), code=302)
 
 if __name__ == '__main__':
     app.run(debug=True)
